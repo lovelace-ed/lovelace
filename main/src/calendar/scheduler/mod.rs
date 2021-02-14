2ad99a60a460ae78643af27a5d05934292e817b9
teymour-aldridge <teymour.aldridge@icloud.com>
2021-02-14 20:43:50 +0000

    Remove redundant clone.

--- main/src/calendar/scheduler/mod.rs
+++ main/src/calendar/scheduler/mod.rs
@@ -135,7 +135,7 @@ pub async fn two_week_schedule(user_id: i32, conn: &Database) -> Result<(), Sche
                         ),
                         DavClient::new_oauth(
                             user_calendar_url,
-                            gcal.access_token.clone(),
+                            gcal.access_token,
                         ),
                     )
                 }
