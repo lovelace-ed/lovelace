1fc72b76b55c67984e4b4af15048a1313bf2f8d2
teymour-aldridge <teymour.aldridge@icloud.com>
2021-02-14 20:41:26 +0000

    Fix doc comments.

--- utils/portia/src/colour.rs
+++ utils/portia/src/colour.rs
@@ -1,17 +1,11 @@
 /// Colous. Note that these may or may not be suitable for your application.
 
 #[derive(CSS, Debug)]
-#[mercutio(
-    css(background_color = "#f4d03f"),
-    elements(Div)
-)]
+#[mercutio(css(background_color = "#f4d03f"), elements(Div))]
 /// Applies a yellow background to a `Div`.
 pub struct YellowBackground;
 
 #[derive(CSS, Debug)]
-#[mercutio(
-    css(background_color = "#d5dbdb"),
-    elements(Div)
-)]
+#[mercutio(css(background_color = "#d5dbdb"), elements(Div))]
 /// Applies a grey background to a `Div`.
 pub struct GreyBackground;
